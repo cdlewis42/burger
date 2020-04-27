cd5c0ba5a3f37a13fbe3d7548dc7b82c8841567a
Celeste Lewis <cdlewis42@gmail.com>
2020-04-26 22:27:20 -0400

    working on JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,24 @@
-var mysql = require("mysql");
+var mysql = require("mysql")
+require("dotenv").config()
 
+var connection;
+//Database is JawsDB on Heroku
+
+//Database is local
+if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "celeste",
-  password: "1234",
+  user: process.env.username,
+  password: process.env.password,
   database: "burgers_db"
 });
+}
+
 
 
 connection.connect(function(err) {
